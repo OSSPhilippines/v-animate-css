8485137918753166be9bf22a62ab9a87f7ee5ee5
Joff Tiquez <jofftiquez@gmail.com>
2022-11-23 09:26:19 +0800

    refactor: add new option for using local css instead of cdn

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import directives from './directives';
 
-var link = document.createElement('link');
-link.rel = 'stylesheet';
-link.href = 'https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css' ;
-document.getElementsByTagName('head')[0].appendChild(link);
-
 const VAnimateCss = {
-  install(Vue, options) {
+  install (Vue, options = {}) {
+    const animateCSSPath = options.animateCSSPath;
+    const link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.href = animateCSSPath || 'https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css' ;
+    document.getElementsByTagName('head')[0].appendChild(link);
+    
     directives(Vue);
-  }
-}
+  },
+};
 
 export default VAnimateCss;
