a94185eea305ff87169b72a3bedf91f2ac905c06
MahoMuri <paolo.santos_12@yahoo.com.ph>
2023-01-18 11:49:58 +0800

    fix: added constant variable for CDN

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
-import directives from "./directives";
+import directives from './directives';
+
+const ANIMATE_CSS_CDN_LINK =
+  'https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css';
 
 const VAnimateCss = {
   install (Vue, options = {}) {
     const animateCSSPath = options.animateCSSPath;
-    const link = document.createElement("link");
-    link.rel = "stylesheet";
-    link.href =
-      animateCSSPath ||
-      "https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css";
-    document.getElementsByTagName("head")[0].appendChild(link);
+    const link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.href = animateCSSPath || ANIMATE_CSS_CDN_LINK;
+    document.getElementsByTagName('head')[0].appendChild(link);
 
     directives(Vue);
   },
